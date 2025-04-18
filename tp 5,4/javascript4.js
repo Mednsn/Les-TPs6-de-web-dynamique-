65e34b8b2652b0ae6fca630384237c34072454e4
Mohamed <mossmoss4000@gmail.com>
2025-04-18 22:27:47 +0100

    Update javascript4.js

--- tp 5,4/javascript4.js
+++ tp 5,4/javascript4.js
@@ -1,18 +1,46 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const maDiv = document.createElement("div");
-    maDiv.className = "ma-div";
-  
-    const monParagraphe = document.createElement("p");
-    monParagraphe.textContent = "Ceci est un paragraphe";
-    maDiv.appendChild(monParagraphe);
-    document.body.appendChild(maDiv);
-  
-    monParagraphe.textContent = "Le texte a été modifié";
-    monParagraphe.style.backgroundColor = "lightblue";
-    monParagraphe.style.textAlign = "center";
-  
-    maDiv.addEventListener("click", function () {
-      monParagraphe.textContent = "Un clic a été détecté";
-    });
-  });
-  
+const form = document.getElementById('todo-form');
+const input = document.getElementById('todo-input');
+const list = document.getElementById('todo-list');
+
+function event_to_do(event) {
+  event.preventDefault();
+
+  const taskText = input.value.trim();
+  if (taskText !== "") {
+      
+    const listItem = document.createElement('li');
+    listItem.classList.add('todo-item');
+    listItem.textContent = taskText;
+
+    const task_button = document.createElement('div');
+    task_button.classList.add('task-button');
+
+
+    const completeButton = document.createElement('button');
+    completeButton.textContent = 'Complete';
+    completeButton.classList.add('complete-button');
+    function completeTask() {
+      listItem.classList.toggle('completed');
+    }
+    completeButton.addEventListener('click', completeTask);
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.classList.add('delete-button');
+    function deleteTask() {
+      listItem.remove();
+    } 
+
+    deleteButton.addEventListener('click', deleteTask);
+
+    task_button.appendChild(completeButton);
+    task_button.appendChild(deleteButton);
+    listItem.appendChild(task_button);
+    list.appendChild(listItem);
+
+    input.value = '';
+
+    }
+}
+
+  form.addEventListener('submit', event_to_do);
